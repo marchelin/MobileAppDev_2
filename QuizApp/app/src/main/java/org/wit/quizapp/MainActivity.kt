b5fa3fcf4726034c754aa98723c14506413ccc9d
Marcelo <marcelolopezdelerma@gmail.com>
2021-12-17 03:06:07 +0100

    Second Commit
    Night Mode implemented.

--- QuizApp/app/src/main/java/org/wit/quizapp/MainActivity.kt
+++ QuizApp/app/src/main/java/org/wit/quizapp/MainActivity.kt
@@ -1,10 +1,12 @@
 package org.wit.quizapp
 
 import android.content.Intent
+import android.content.SharedPreferences
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Toast
+import androidx.appcompat.app.AppCompatDelegate
 import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity()
@@ -15,6 +17,34 @@ class MainActivity : AppCompatActivity()
         setContentView(R.layout.activity_main)
 
         window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+
+        val appSettingPrefs: SharedPreferences = getSharedPreferences("AppSettingPrefs", 0)
+        val sharedPrefsEdit: SharedPreferences.Editor = appSettingPrefs.edit()
+        val isNightModeOn: Boolean = appSettingPrefs.getBoolean("NightMode", false)
+
+        if(isNightModeOn){
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+            //switch_btn.text = "Disable Dark Mode"
+        } else {
+            AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+            //switch_btn.text = "Enable Dark Mode"
+        }
+
+        switch_btn.setOnClickListener(View.OnClickListener {
+            if(isNightModeOn){
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_NO)
+                sharedPrefsEdit.putBoolean("NightMode", false)
+                sharedPrefsEdit.apply()
+
+                //switch_btn.text = "Enable Dark Mode"
+            } else {
+                AppCompatDelegate.setDefaultNightMode(AppCompatDelegate.MODE_NIGHT_YES)
+                sharedPrefsEdit.putBoolean("NightMode", true)
+                sharedPrefsEdit.apply()
+
+                //switch_btn.text = "Disable Dark Mode"
+            }
+        })
 
         btn_start.setOnClickListener {
 
